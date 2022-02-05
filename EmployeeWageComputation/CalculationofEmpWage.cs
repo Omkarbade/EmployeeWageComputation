fe6f71aeedd2c183ed9808f6a4573e2916a4f12f
Omkarbade <omakrbade3799@gmail.com>
2022-02-06 00:53:44 +0530

    [Omkar] Add PartTime program

---
+++ EmployeeWageComputation/CalculationofEmpWage.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    public class CalculationofEmpWage
+    {
+        public static void EmpWageCompute() // Calling Static method 
+        {
+            int fulltime = 2;
+            int partime = 1;
+            int totalWage = 0;
+            int empWageHrs = 0;
+            int empWagePerHrs = 20;
+            Random random = new Random();           //Creating Object of Random Class
+            int EmpAttendence = random.Next(0, 3); //Generating random number between 0 and 2
+
+            if (EmpAttendence == fulltime)  // Using if else statement to check the condition
+            {
+                Console.WriteLine("Employee is Present");
+                empWageHrs = 8;
+            }
+            else if (EmpAttendence == partime)
+            {
+                Console.WriteLine("Employee is present");
+                empWagePerHrs = 4;
+            }
+            else
+                Console.WriteLine("Employee is absent");
+
+            totalWage = empWageHrs * empWagePerHrs;
+            Console.WriteLine("Employee total Daily Wage: " + totalWage);
+        }
+
+    }
+ }
+
+
