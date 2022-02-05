0619999fd1752f889e89ff763d5c0773557e0c11
Omkarbade <omakrbade3799@gmail.com>
2022-02-06 00:49:30 +0530

    [Omkar] Add DailyWage program

---
+++ EmployeeWageComputation/CalculationofEmpWage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    public class CalculationofEmpWage
+    {
+        public static void EmpWageCompute() // Calling Static method 
+        {
+            int present = 1;
+            int totalWage = 0;
+            int empWageHrs = 0;
+            int empWagePerHrs = 20;
+            Random random = new Random();           // Creating Object of Random Class
+            int EmpAttendence = random.Next(0, 2); // Generating random number between 0 and 1
+
+            if (EmpAttendence == present)  // Using if else statement to check the condition
+            {
+                Console.WriteLine("Employee is Present");
+                empWageHrs = 8;
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+
+            totalWage = empWageHrs * empWagePerHrs;
+            Console.WriteLine("EMployee total daily wage: " + totalWage);
+        }
+
+    }
+ }
+
+
