64884338e810f955e7c5cae9bbbd7c4284b0e0a0
Omkarbade <omakrbade3799@gmail.com>
2022-02-06 00:10:17 +0530

    [Omkar] Add EmpAttendance program

---
+++ EmployeeWageComputation/CalculationofEmpWage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    public class CalculationofEmpWage
+    {
+        
+         
+        public static void WageCompute()
+        {
+            int present = 1;
+            Random random = new Random();           //Creating Object of Random Class
+            int empAttendence = random.Next(0, 2); //Generating random number between 0 and 1
+
+
+            if (empAttendence == present) //Checking condition using if else statement
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is not Present");
+            }
+           
+        }
+    }
+}
+
