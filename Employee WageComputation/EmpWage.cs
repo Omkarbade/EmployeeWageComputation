d5d0aa76f33f14dacbf0f7691d4f25f32a383c83
Omkarbade <omakrbade3799@gmail.com>
2022-02-07 00:02:03 +0530

    [Omkar] Add manage empwage company program

---
+++ Employee WageComputation/EmpWage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_WageComputation
+{
+    internal class EmpWage
+    {
+        public string company;
+        public int empRatePerHr, maxWorkingDays, maxWorkingHrs, totalWage;
+        // constructor to initialize variable
+        public EmpWage(string company, int empRatePerHr, int maxWorkingDays, int maxWorkingHrs)
+        {
+            this.company = company;
+            this.empRatePerHr = empRatePerHr;
+            this.maxWorkingDays = maxWorkingDays;
+            this.maxWorkingHrs = maxWorkingHrs;
+        }
+        public void SetTotalWage(int totalWage)
+        {
+            this.totalWage = totalWage;
+        }
+    }
+}
